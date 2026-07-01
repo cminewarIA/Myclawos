a6b8782403197989d265cada79bd4daad25fb40b
cminewarIA <cminewar1.5@gmail.com>
2026-07-01 21:35:36 +0100

    ci: automate GitHub releases and update version
    Increment build version to 1.2.24 and add an automated workflow step to publish build artifacts to GitHub releases upon successful completion.

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const VERSION = "1.2.20";
-export const BUILD_NUMBER = 20;
-export const LAST_UPDATED = "2026-07-01T20:31:31.778Z";
+export const VERSION = "1.2.24";
+export const BUILD_NUMBER = 24;
+export const LAST_UPDATED = "2026-07-01T20:34:04.191Z";
